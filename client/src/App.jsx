a42b02b58d1438f0b4b92d4f0bbfca56169a7eb8
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-08 16:36:14 +0530

    Basic of Routing

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Dashboard from './components/Dashboard';
+import Landing from './components/Landing';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/> } />
+          <Route path="/" element={<Landing/> } />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
